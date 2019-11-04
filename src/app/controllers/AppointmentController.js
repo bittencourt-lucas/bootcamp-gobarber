dd8840750b54ab923f680358b4e5afb759d9d123
Lucas Bittencourt <lucascafieiro@hotmail.com>
2019-11-03 23:03:01 -0300

    Added Notifications with MongoDB

--- src/app/controllers/AppointmentController.js
+++ src/app/controllers/AppointmentController.js
@@ -1,8 +1,9 @@
 import * as Yup from 'yup';
-import { startOfHour, parseISO, isBefore } from 'date-fns';
+import { startOfHour, parseISO, isBefore, format } from 'date-fns';
 import Appointment from '../models/Appointment';
 import User from '../models/User';
 import File from '../models/File';
+import Notification from '../schemas/Notification';
 
 class AppointmentController {
   async index(req, res) {
@@ -88,6 +89,18 @@ class AppointmentController {
       user_id: req.userId,
       provider_id,
       date: hourStart,
+    });
+
+    /**
+     * Notify appointment provider
+     */
+    const user = await User.findByPk(req.userId);
+    const formattedDate = format(hourStart,
+      "'day' dd 'of' MMMM', at' H:mm'h'");
+
+    await Notification.create({
+      content: `New appointment scheduled for ${user.name} for ${formattedDate}`,
+      user: provider_id,
     });
 
     return res.json(appointment);
