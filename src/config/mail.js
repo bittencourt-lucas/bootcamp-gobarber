ba8168ace6ecd6defb9d0e1e0f8c19b295759472
bittencourt-lucas <lucascafieiro@hotmail.com>
2019-12-04 22:01:26 -0300

    Mail notifications with layout added

---
+++ src/config/mail.js
@@ -0,0 +1,12 @@
+export default {
+  host: 'smtp.mailtrap.io',
+  port: '2525',
+  secure: false,
+  auth: {
+    user: '8870abc1ef0ae3',
+    pass: '4fe585290a6354',
+  },
+  default: {
+    from: 'Equipe GoBarber <noreply@gobarber.com>',
+  },
+};
